70ee80fc56bc6c5f8261b26c81c580c23ff138d1
PrajwalGpy <prajwalgpy@gmail.com>
2026-07-09 16:41:37 +0530

    2 new query added

---
+++ tests/orders.spec.js
@@ -0,0 +1,14 @@
+const { test, expect } = require("@playwright/test");
+const { executeQuery } = require("../database/db");
+
+test("Verify order exists", async () => {
+  const result = await executeQuery(`
+        SELECT *
+        FROM orders
+        WHERE order_id=1;
+        `);
+  console.log(result);
+  expect(result.rows.length).toBe(1);
+
+  expect(result.rows[0].order_status).toBe("Completed");
+});
---
+++ tests/payments.spec.js
@@ -0,0 +1,11 @@
+const { test, expect } = require("@playwright/test");
+
+const { executeQuery } = require("../database/db");
+
+test("Verify payment status", async () => {
+  const result =await executeQuery(`
+            SELECT * FROM payments WHERE order_id=1;
+            `);
+  console.log(result.rows);
+  expect(result.rows[0].payment_status).toBe("Paid");
+});
