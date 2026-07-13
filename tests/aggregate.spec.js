a38650bdab51d6a5be595eb2108146207774f980
PrajwalGpy <prajwalgpy@gmail.com>
2026-07-13 08:14:21 +0530

    Aggregate

---
+++ tests/aggregate.spec.js
@@ -0,0 +1,10 @@
+const { test, expect } = require("@playwright/test");
+const { executeQuery } = require("../database/db");
+
+test("Total payment amount", async () => {
+  const result = await executeQuery(`
+         SELECT SUM(amount) total
+        FROM payments;`);
+    console.log(result)
+    expect(Number(result.rows[0].total)).toBeGreaterThan(100000);
+});
