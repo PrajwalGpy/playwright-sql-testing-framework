c86e2b4cc1dd6c1f034f8b02ca5997ec800b2c89
PrajwalGpy <prajwalgpy@gmail.com>
2026-07-13 10:06:34 +0530

    added

--- playwright.config.js
+++ playwright.config.js
@@ -1,22 +1,21 @@
 // @ts-check
-const { defineConfig, devices } = require('@playwright/test');
+const { defineConfig, devices } = require("@playwright/test");
 
 module.exports = defineConfig({
-  testDir: './tests',
-  testMatch: ['**/*.spec.js'],
+  testDir: "./tests",
+  testMatch: ["**/*.spec.js"],
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [["line"], ["html"], ["allure-playwright"]],
   use: {
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
   ],
 });
-
