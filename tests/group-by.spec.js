315a748f1526458474ccec508ec87d099888a846
PrajwalGpy <prajwalgpy@gmail.com>
2026-07-13 08:19:35 +0530

    groupby.

---
+++ tests/group-by.spec.js
@@ -0,0 +1,16 @@
+const { test, expect } = require("@playwright/test");
+const { executeQuery } = require("../database/db");
+
+test("Customer Order Count", async () => {
+  const result = await executeQuery(`
+    SELECT
+    customer_id,
+    COUNT(*) total_orders
+    FROM orders
+    GROUP BY customer_id
+    ORDER BY customer_id;
+    `);
+
+    console.log(result)
+  expect(result.rows.length).toBeGreaterThan(0);
+});
